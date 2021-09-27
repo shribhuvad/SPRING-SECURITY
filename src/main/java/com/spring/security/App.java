04b6ac7cd0ed0a96e254e6aa61bc22fc606dd885
shribhuvad <34307834+shribhuvad@users.noreply.github.com>
2021-09-27 18:30:53 +0530

    jenkins Test

--- src/main/java/com/spring/security/App.java
+++ src/main/java/com/spring/security/App.java
@@ -10,8 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App 
 {
+	
+	
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args); 
+        System.out.println("SPRING SERVER STARTED");
     }
 }
